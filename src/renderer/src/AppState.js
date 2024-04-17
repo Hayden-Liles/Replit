e4fe2baccfacd71216922fe27ec9ac6058f4849a
Hayden Liles <lileshaydenreal@gmail.com>
2024-04-17 16:54:40 -0600

    first commit

---
+++ src/renderer/src/AppState.js
@@ -0,0 +1,26 @@
+import { reactive, watch, watchEffect } from 'vue'
+
+    export const AppState = reactive({
+        // /** @type {import('./models/Poke').Poke[]} */
+        // pokes: [],
+    })
+
+    // watchEffect(() => {
+    //     if (window.electron) {
+    //         window.electron.loadAppState()
+    //         window.electron.onAppStateLoaded((loadedAppState) => {
+    //             Object.assign(AppState, loadedAppState)
+    //         })
+    //     }
+    // })
+
+    // watch(
+    //     () => AppState,
+    //     () => {
+    //         if (window.electron) {
+    //             const plainAppState = JSON.parse(JSON.stringify(AppState));
+    //             window.electron.saveAppState(plainAppState)
+    //         }
+    //     },
+    //     { deep: true }
+    // )
---
+++ src/renderer/src/pages/Home.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="app-container">
+        <splitpanes style="height: 100vh;">
+
+            <pane size="20">
+                <div class="container-fluid m-0 p-0 test">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="explorer">Explorer</div>
+                        </div>
+                    </div>
+                </div>
+            </pane>
+            <pane>
+
+                <splitpanes horizontal>
+                    <pane size="70">
+                        <div class="editor h-100 w-100" ref="editorElement"></div>
+                    </pane>
+
+                    <pane size="30">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12">
+                                    Terminal
+                                </div>
+                            </div>
+                        </div>
+                    </pane>
+
+                </splitpanes>
+            </pane>
+        </splitpanes>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as monaco from 'monaco-editor';
+import { Splitpanes, Pane } from 'splitpanes';
+import 'splitpanes/dist/splitpanes.css';
+
+const editorElement = ref(null);
+
+onMounted(() => {
+    if (editorElement.value) {
+        monaco.editor.create(editorElement.value, {
+            value: '// type your code...',
+            language: 'javascript',
+            theme: 'vs-dark',
+            automaticLayout: true
+        });
+    }
+});
+</script>
+
+<style scoped>
+.app-container {
+    height: 100vh;
+}
+
+
+</style>
